9a253709099f94f1ad86c43797ed4ae4dc61802e
Jonah Pearl <j4jonahp@gmail.com>
2023-04-28 12:45:00 -0400

    minor

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='top_bottom_triggered',
       version='0.1',
       description='top bottom recording code with pyk4a',
-      author='Caleb Weinreb',
+      author='Caleb Weinreb and Jonah Pearl',
       license='MIT',
       packages=['top_bottom_triggered'],
       zip_safe=False)
